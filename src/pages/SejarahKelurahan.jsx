aa8cfe9d3cb18d9b4a077a8324b0819ae11f65c6
AbathTH <bomabeat46@gmail.com>
2024-09-07 22:30:57 +0800

    Update Sejarah Kelurahan + Add StyleCostum.css

--- src/pages/SejarahKelurahan.jsx
+++ src/pages/SejarahKelurahan.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./Layout";
+import './StyleCostum.css';
 
 
 const SejarahKelurahan = () => {
@@ -75,151 +76,162 @@ const SejarahKelurahan = () => {
         <div className="container mt-4">
             <div className="card-body mx-auto col-lg-12 shadow-lg rounded-3 rectangle3">
             <p>Daftar Pemimpin pada Masa Kelurahan :</p>
+            <div className="table-responsive">
+
+            
             <table className="table table-bordered text-center">
                 <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Nama Pejabat</th>
-                    <th>Jabatan</th>
-                    <th>Tahun</th>
-                </tr>
+                    <tr>
+                        <th>No.</th>
+                        <th>Nama Pejabat</th>
+                        <th>Jabatan</th>
+                        <th>Tahun</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {/* * data ke-1 */}
+            {/* <!-- * data ke-1 --> */}
                 <tr>
                     <td>1</td>
                     <td>JUNUS DETHAN</td>
                     <td>Tamukung/Kepala Desa</td>
                     <td>1881-1910</td>
                 </tr>
-                {/* * data ke-2 */}
+            {/* <!-- * data ke-2 --> */}
                 <tr>
                     <td>2</td>
                     <td>HENDRIK JESUA</td>
                     <td>Tamukung/Kepala Desa</td>
                     <td>1910-1929</td>
                 </tr>
-                {/* * data ke-3 */}
+            {/* <!-- * data ke-3 --> */}
                 <tr>
                     <td>3</td>
                     <td>SIMON NDOEN</td>
                     <td>Tamukung/Kepala Desa</td>
                     <td>1929-1942</td>
                 </tr>
-                {/* * data ke-4 */}
+            {/* <!-- * data ke-4 --> */}
                 <tr>
                     <td>4</td>
                     <td>THOMAS DETHAN</td>
                     <td>Tamukung/Kepala Desa</td>
                     <td>1942-1944</td>
                 </tr>
-                {/* * data ke-5 */}
+            {/* <!-- * data ke-5 --> */}
                 <tr>
                     <td>5</td>
                     <td>WILEM A. TAULO</td>
                     <td>Tamukung/Kepala Desa</td>
                     <td>1944-1967</td>
                 </tr>
-                {/* * data ke-6 */}
-                <tr>
-                    <td>6</td>
-                    <td>E. PELLO</td>
-                    <td>Kepala Desa</td>
-                    <td>1967-1972</td>
-                </tr>
-                {/* * data ke-7 */}
-                <tr>
-                    <td>7</td>
-                    <td>JAP H. TAULO</td>
-                    <td>Kepala Desa</td>
-                    <td>1972-1987</td>
-                </tr>
-                {/* * data ke-8 */}
-                <tr>
-                    <td>8</td>
-                    <td>R. IZAACH</td>
-                    <td>Caretaker</td>
-                    <td>1987-1988</td>
-                </tr>
-                {/* * data ke-9 */}
-                <tr>
-                    <td>9</td>
-                    <td>Z. MIRAMANGI</td>
-                    <td>Caretaker</td>
-                    <td>1988-Juli 1988</td>
-                </tr>
-                {/* * data ke-10 */}
-                <tr>
-                    <td>10</td>
-                    <td>H. BAOEN</td>
-                    <td>LURAH</td>
-                    <td>1988-20 Maret 1995</td>
-                </tr>
-                {/* * data ke-11 */}
-                <tr>
-                    <td>11</td>
-                    <td>BENYAMIN NDUN, SH</td>
-                    <td>LURAH</td>
-                    <td>20 Maret 1995 – 06 Februari 1999</td>
-                </tr>
-                {/* * data ke-12 */}
-                <tr>
-                    <td>12</td>
-                    <td>AGUS RIRIMASE, Ap.</td>
-                    <td>LURAH</td>
-                    <td>06 Februari 1999 – 31 Juli 2000</td>
-                </tr>
-                {/* * data ke-13 */}
-                <tr>
-                    <td>13</td>
-                    <td>ORSON G. NAWA, SH</td>
-                    <td>LURAH</td>
-                    <td>15 Desember 2000 – Oktober 2004</td>
-                </tr>
-                {/* * data ke-14 */}
-                <tr>
-                    <td>14</td>
-                    <td>G. P. NITBANI, SH</td>
-                    <td>LURAH</td>
-                    <td>Oktober 2004 – 10 April 2007</td>
-                </tr>
-                {/* * data ke-15 */}
-                <tr>
-                    <td>15</td>
-                    <td>SETH F. MESAH</td>
-                    <td>LURAH</td>
-                    <td>10 April 2007 – 15 Oktober 2009</td>
-                </tr>
-                {/* * data ke-16 */}
-                <tr>
-                    <td>16</td>
-                    <td>G. P. NITBANI, SH</td>
-                    <td>LURAH</td>
-                    <td>15 Oktober 2009 – Maret 2012</td>
-                </tr>
-                {/* * data ke-17 */}
-                <tr>
-                    <td>17</td>
-                    <td>SETH F. MESAH</td>
-                    <td>LURAH</td>
-                    <td>Maret 2012 – Oktober 2014</td>
-                </tr>
-                {/* * data ke-18 */}
-                <tr>
-                    <td>18</td>
-                    <td>JAMES MAUSI, S. Sos</td>
-                    <td>LURAH</td>
-                    <td>Oktober 2014 – September 2021</td>
-                </tr>
-                {/* * data ke-19 */}
-                <tr>
-                    <td>19</td>
-                    <td>LEONARD MESAH, S. Sos</td>
-                    <td>LURAH</td>
-                    <td>September 2021 – Sekarang</td>
-                </tr>
-                </tbody>
+            {/* <!-- * data ke-6 --> */}
+            <tr>
+                <td>6</td>
+                <td>E. PELLO</td>
+                <td>Kepala Desa</td>
+                <td>1967-1972</td>
+            </tr>
+            {/* <!-- * data ke-7 --> */}
+            <tr>
+                <td>7</td>
+                <td>JAP H. TAULO</td>
+                <td>Kepala Desa</td>
+                <td>1972-1987</td>
+            </tr>
+            {/* <!-- * data ke-8 --> */}
+            <tr>
+                <td>8</td>
+                <td>R. IZAACH</td>
+                <td>Caretaker</td>
+                <td>1987-1988</td>
+            </tr>
+            {/* <!-- * data ke-9 --> */}
+            <tr>
+                <td>9</td>
+                <td>Z. MIRAMANGI</td>
+                <td>Caretaker</td>
+                <td>1988-Juli 1988</td>
+            </tr>
+            {/* <!-- * data ke-10 --> */}
+            <tr>
+                <td>10</td>
+                <td>H. BAOEN</td>
+                <td>LURAH</td>
+                <td>1988-20 Maret 1995</td>
+            </tr>
+            {/* <!-- * data ke-11 --> */}
+            <tr>
+                <td>11</td>
+                <td>BENYAMIN NDUN, SH</td>
+                <td>LURAH</td>
+                <td>20 Maret 1995 – 06 Februari 1999</td>
+            </tr>
+            {/* <!-- * data ke-12 --> */}
+            <tr>
+                <td>12</td>
+                <td>AGUS RIRIMASE, Ap.</td>
+                <td>LURAH</td>
+                <td>06 Februari 1999 – 31 Juli 2000</td>
+            </tr>
+            {/* <!-- * data ke-13 --> */}
+            <tr>
+                <td>13</td>
+                <td>ORSON G. NAWA, SH</td>
+                <td>LURAH</td>
+                <td>15 Desember 2000 – Oktober 2004</td>
+            </tr>
+            {/* <!-- * data ke-14 --> */}
+            <tr>
+                <td>14</td>
+                <td>JULIANUS D. BENU, S.Sos</td>
+                <td>LURAH</td>
+                <td>Oktober 2004 – Januari 2008</td>
+            </tr>
+            {/* <!-- * data ke-15 --> */}
+            <tr>
+                <td>15</td>
+                <td>ARIANTJE TAULO</td>
+                <td>Plt. Lurah</td>
+                <td>Januari 2008 – April 2008</td>
+            </tr>
+            {/* <!-- * data ke-16 --> */}
+            <tr>
+                <td>16</td>
+                <td>REGINA D. CARVALHO</td>
+                <td>LURAH</td>
+                <td>April 2008 – November 2008</td>
+            </tr>
+            {/* <!-- * data ke-17 --> */}
+            <tr>
+                <td>17</td>
+                <td>MARIA A. SALENSI, SE</td>
+                <td>LURAH</td>
+                <td>Desember 2008 – Desember 2018</td>
+            </tr>
+            {/* <!-- * data ke-18 --> */}
+            <tr>
+                <td>18</td>
+                <td>GEREGORIUS E. ROHI, S.STP</td>
+                <td>LURAH</td>
+                <td>Desember 2018 – Juni 2021</td>
+            </tr>
+            {/* <!-- * data ke-19 --> */}
+            <tr>
+                <td>19</td>
+                <td>WILLIAM M. DIMA, S.A.P</td>
+                <td>LURAH</td>
+                <td>Juni 2021 - Januari 2024</td>
+            </tr>
+            {/* <!-- * data ke-20 --> */}
+            <tr>
+                <td>20</td>
+                <td>IBRAHIM HERMANTO PASSOE, S.SOS</td>
+                <td>Plt.Lurah</td>
+                <td>Januari 2024 – sekarang</td>
+            </tr>
+            </tbody>
             </table>
+            </div>
             </div>
         </div>
         </div>
