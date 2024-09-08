b8cf8ed04c27e8b82ea946e4ac2bf76422bb239a
AbathTH <bomabeat46@gmail.com>
2024-09-08 22:17:19 +0800

    Update File Struktur Organisasi Dengan PNG

--- src/pages/StrukturOrganisasi.jsx
+++ src/pages/StrukturOrganisasi.jsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import React from 'react';
 import Layout from './Layout';
-import strukturPDF from '../assets/Struktur-Oetete.pdf';
-
-// Explicitly specify the worker source for react-pdf
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import strukturGambar from '../assets/Struktur Organisasi OEBA.png';
 
 function StrukturOrganisasi() {
-  const [numPages, setNumPages] = useState(null);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
     <Layout>
       <div className="container mt-5 mb-5">
         <h2 className="text-center mb-4">Struktur Organisasi</h2>
         <div className="d-flex justify-content-center">
-          <Document
-            file={strukturPDF}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
+          <img 
+            src={strukturGambar} 
+            alt="Struktur Organisasi" 
+            style={{ width: '100%', maxWidth: '800px', height: 'auto' , border: '5px solid #000', borderRadius: '10px'}} 
+          />
         </div>
       </div>
     </Layout>
